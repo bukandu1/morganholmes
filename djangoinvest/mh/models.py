7a48e7e341f6186cfa197839fc06b7dcff2b5c9b
Beverly N Ukandu <beverlyukandu@gmail.com>
2020-01-19 04:44:18 -0500

    Updated gitignore to remove pandas testing and PayPal data.

--- djangoinvest/mh/models.py
+++ djangoinvest/mh/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+#Investors
+#Payments
+
+class Investor(models.Model):
+    name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
