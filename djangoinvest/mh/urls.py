a4b51c5e1aefe6dc4c262e7283f751f9d55e9490
Beverly N Ukandu <beverlyukandu@gmail.com>
2020-01-19 00:31:24 -0500

    Created new templates for overview and moved Jinja templating over to new overview template.

--- djangoinvest/mh/urls.py
+++ djangoinvest/mh/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='mh-home'),
     path('about/', views.about, name='mh-about'),
+    path('overview/', views.overview, name='mh-overview'),
 ]
--- djangoinvest/mh/views.py
+++ djangoinvest/mh/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+# Test data for main page
+investors = [
+    {
+        'name': 'ChrisDC',
+        'payments': '$200',
+        'date_posted': 'January 18, 2020'
+    },
+    {
+        'name': 'BevNU',
+        'payments': '$1200',
+        'date_posted': 'January 20, 2020'
+    },
+]
 
 # Home page
 def home(request):
@@ -9,3 +22,9 @@ def home(request):
 
 def about(request):
     return render(request, 'mh/about.html')
+
+def overview(request):
+    context = {
+        'investors': investors
+    }
+    return render(request, 'mh/overview.html', context)
